66bf725d7dfe10257217e36339f4208b33dfc225
Eake123 <82546019+Eake123@users.noreply.github.com>
2022-12-08 10:56:43 -0700

    Update python_to_vba.py

--- python_to_vba.py
+++ python_to_vba.py
@@ -2,7 +2,6 @@ import json
 from datetime import datetime
 import pandas as pd
 import subprocess 
-from config_script import config_python_to_vba
 import string
 def int_to_col(column_int:int):
     start_index = 1   #  it can start either at 0 or at 1
